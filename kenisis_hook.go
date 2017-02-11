c2cee355c0b630c5f6c2c169bdf541b32c9f8b06
Abdul Dakkak <abduld@wolfram.com>
2017-02-11 17:26:47 -0600

    set name of stream to app name

--- kenisis_hook.go
+++ kenisis_hook.go
@@ -3,11 +3,12 @@ package logger
 import (
 	"github.com/Sirupsen/logrus"
 	kinesis "github.com/evalphobia/logrus_kinesis"
+	"github.com/rai-project/config"
 	"github.com/spf13/viper"
 )
 
 func setupKenisisHook(log *Logger) {
-	hook, err := kinesis.New("my_stream", kinesis.Config{
+	hook, err := kinesis.New(config.App.Name, kinesis.Config{
 		AccessKey: viper.GetString("aws.access_key_id"),
 		SecretKey: viper.GetString("aws.secret_access_key"),
 		Region:    viper.GetString("aws.region"),
