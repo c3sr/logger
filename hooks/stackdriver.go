1bbbd84a77d910861edee179cc567fafc54fed0e
Abdul Dakkak <abduld@wolfram.com>
2017-03-18 10:16:40 +0000

    work on logger

--- hooks/stackdriver.go
+++ hooks/stackdriver.go
@@ -3,13 +3,19 @@ package hooks
 import (
 	"github.com/knq/sdhook"
 	"github.com/rai-project/config"
+	"github.com/rai-project/googlecloud"
 	"github.com/rai-project/logger"
 )
 
 func init() {
 	config.AfterInit(func() {
+		googlecloud.Config.Wait()
+
+		opts := googlecloud.NewOptions()
+
 		h, err := sdhook.New(
 			sdhook.GoogleLoggingAgent(),
+			sdhook.GoogleServiceAccountCredentialsJSON(opts.Bytes()),
 			sdhook.ErrorReportingService(config.App.Name),
 		)
 		if err != nil {
