5219a1cd5fdd85612bb0d5d9773a810b10fab8fe
Abdul Dakkak <dakkak@illinois.edu>
2017-05-04 02:55:02 -0500

    fix kinesis logging

--- hooks/kinesis.go
+++ hooks/kinesis.go
@@ -1,6 +1,8 @@
 package hooks
 
 import (
+	"github.com/Sirupsen/logrus"
+	aaws "github.com/aws/aws-sdk-go/aws"
 	"github.com/evalphobia/logrus_kinesis"
 	"github.com/rai-project/aws"
 	"github.com/rai-project/config"
@@ -14,17 +16,39 @@ func init() {
 		if !logger.UsingHook("kinesis") {
 			return
 		}
+		println("setting up kinesis logger hook @", config.App.Name)
 
 		aws.Config.Wait()
 
 		c, err := aws.NewConfig()
 		if err != nil {
+			println("failed to load kinesis logger hook ", err)
 			return
 		}
-		h, err := logrus_kinesis.NewWithAWSConfig(config.App.Name, c)
+		cred, err := c.Credentials.Get()
 		if err != nil {
+			println("failed to load kinesis logger hook ", err)
 			return
 		}
+		h, err := logrus_kinesis.New(config.App.Name, logrus_kinesis.Config{
+			AccessKey: cred.AccessKeyID,
+			SecretKey: cred.SecretAccessKey,
+			Region:    aaws.StringValue(c.Region),
+			Endpoint:  aaws.StringValue(c.Endpoint),
+		})
+		if err != nil {
+			println("failed to load kinesis logger hook ", err)
+			return
+		}
+		h.Async()
+		h.SetLevels([]logrus.Level{
+			logrus.PanicLevel,
+			logrus.FatalLevel,
+			logrus.ErrorLevel,
+			logrus.WarnLevel,
+			logrus.InfoLevel,
+			logrus.DebugLevel,
+		})
 		logger.RegisterHook("kinesis", h)
 	})
 }
