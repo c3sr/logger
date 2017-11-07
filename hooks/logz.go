f65a0f4f6292da5a4d1408b43570ea8cbbcf9bc8
Abdul Dakkak <abduld@wolfram.com>
2017-11-07 10:53:16 -0600

    add logz support

---
+++ hooks/logz.go
@@ -0,0 +1,39 @@
+package hooks
+
+import (
+	"fmt"
+
+	"github.com/bshuster-repo/logruzio"
+	"github.com/rai-project/config"
+	"github.com/rai-project/logger"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	config.OnInit(func() {
+		logger.Config.Wait()
+
+		if !logger.UsingHook("logz") {
+			return
+		}
+
+		config.App.Wait()
+		logger.Config.Wait()
+
+		token := viper.GetString("logz.token")
+
+		ctx := logrus.Fields{
+			"ID":        config.App.Name,
+			"Version":   config.App.Version.Version,
+			"BuildDate": config.App.Version.BuildDate,
+		}
+		hook, err := logruzio.New(token, config.App.Name, ctx)
+		if err != nil {
+			fmt.Println("cannot register logz hook ", err)
+			return
+		}
+
+		logger.RegisterHook("logz", hook)
+	})
+}
