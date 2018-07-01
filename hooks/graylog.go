4b419c01402195c6bf46a66ada084ca2422c9b60
Abdul Dakkak <abduld@wolfram.com>
2018-06-30 22:37:04 -0500

    add graylog

--- hooks/graylog.go
+++ hooks/graylog.go
@@ -1,3 +1,50 @@
 package hooks
 
-// "gopkg.in/gemnasium/logrus-graylog-hook.v2"
+import (
+	"os"
+
+	"github.com/rai-project/config"
+	"github.com/rai-project/logger"
+	"github.com/rai-project/utils"
+	"github.com/spf13/viper"
+	"gopkg.in/gemnasium/logrus-graylog-hook.v2"
+)
+
+func init() {
+	config.OnInit(func() {
+		config.App.Wait()
+		logger.Config.Wait()
+
+		if !logger.UsingHook("graylog") {
+			return
+		}
+
+		address := decrypt(viper.GetString("graylog.address"))
+		if address == "" {
+			return
+		}
+
+		port := decrypt(viper.GetString("graylog.port"))
+		if port == "" {
+			port = "9000"
+		}
+
+		ctx := map[string]interface{}{
+			"ID":        config.App.Name,
+			"Version":   config.App.Version.Version,
+			"BuildDate": config.App.Version.BuildDate,
+		}
+
+		if ip, err := utils.GetExternalIp(); err == nil {
+			ctx["IP"] = ip
+		}
+
+		if hostname, err := os.Hostname(); err == nil {
+			ctx["HostName"] = hostname
+		}
+
+		hook := graylog.NewGraylogHook(address+":"+port, ctx)
+
+		logger.RegisterHook("graylog", hook)
+	})
+}
